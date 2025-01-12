816df9e4f7b024d8e5f392e658060df60a8c49ea
sutaaa0 <sutadev0@gmail.com>
2025-01-12 11:07:04 +0700

    Add user level check for admin access in dashboard page

--- app/(administrator)/dashboard-admin/page.tsx
+++ app/(administrator)/dashboard-admin/page.tsx
@@ -1,6 +1,15 @@
+import { getCurrentUser } from '@/server/actions'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-const page = () => {
+const page = async () => {
+
+  const currentUser = await getCurrentUser()
+
+  if(currentUser?.level !== 'ADMIN') {
+    redirect('/home')
+  }
+
   return (
     <div className='flex justify-center items-center h-screen'>
         <h1>Halaman Administrator</h1>
