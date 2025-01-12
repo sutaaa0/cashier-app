14edb607c99156bd2f6f7179e704064c1cf9436d
sutaaa0 <sutadev0@gmail.com>
2025-01-13 00:43:59 +0700

    Add remote image pattern for Vercel storage in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ images: {
+    remotePatterns: [
+      {
+        hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com"
+      }
+    ]
+ }
 };
 
 export default nextConfig;
