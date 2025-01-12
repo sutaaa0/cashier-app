cfe311aef26e2d652b929b6e249dbe97d35f822d
sutaaa0 <sutadev0@gmail.com>
2025-01-12 10:20:14 +0700

    Refactor database connection setup to use globalThis and simplify PrismaClient initialization

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,7 @@
-import { PrismaClient } from "@prisma/client";
-
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["query", "info", "warn", "error"],
-  });
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+import { PrismaClient } from "@prisma/client"
+ 
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+ 
+export const prisma = globalForPrisma.prisma || new PrismaClient()
+ 
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
