07dd5a4ad0fb657f4e98584ba397cc0e298f2802
sutaaa0 <sutadev0@gmail.com>
2025-01-12 10:19:52 +0700

    Add administrator dashboard page component

---
+++ app/(administrator)/dashboard-admin/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+        <h1>Halaman Administrator</h1>
+    </div>
+  )
+}
+
+export default page
