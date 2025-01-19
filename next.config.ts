dde1a19a043a9b94b41dd7e81af1ec1300882acb
sutaaa0 <sutadev0@gmail.com>
2025-01-19 14:57:59 +0700

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,20 @@ const nextConfig: NextConfig = {
  images: {
     remotePatterns: [
       {
-        hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com"
-      }, {
-        hostname: "i.pinimg.com"
-      }
+        protocol: "https", // Pastikan menggunakan https
+        hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com", // Hostname pertama
+        pathname: "/**", // Pathway untuk semua gambar
+      },
+      {
+        protocol: "https", // Pastikan menggunakan https
+        hostname: "i.pinimg.com", // Hostname kedua
+        pathname: "/**", // Pathway untuk semua gambar
+      },
+      {
+        protocol: "https", // Pastikan menggunakan https
+        hostname: "res.cloudinary.com", // Hostname Cloudinary
+        pathname: "/**", // Pathway untuk semua gambar
+      },
     ]
  }
 };
