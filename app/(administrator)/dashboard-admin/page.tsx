3367d25dce7e085925dc97850d846d16ae90795c
sutaaa0 <sutadev0@gmail.com>
2025-01-20 23:58:36 +0700

    update page.tsx

--- app/(administrator)/dashboard-admin/page.tsx
+++ app/(administrator)/dashboard-admin/page.tsx
@@ -6,11 +6,11 @@ import { StockManagement } from './components/StockManagement'
 import { AppSettings } from './components/AppSettings'
 import { AnalyticsPage } from './components/AnalyticsPage'
 import { UserManagement } from '@/components/UserManagement'
-import { ProductManagement } from '@/components/ProductManagement'
 import { CustomerManagement } from '@/components/CustomerManagement'
 import { ReportManagement } from './components/RepostManagement'
 import { AdminHeader } from '@/components/AdminHeader'
 import { AdminSidebar } from '@/components/AdminSidebar'
+import { ProductManagement } from '@/components/ProductManagement'
 
 export default function AdminDashboard() {
   const [activeSection, setActiveSection] = useState('users')
