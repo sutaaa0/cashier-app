ccd2f42ff29a4bb112153468f13c6a171960b7e0
sutaaa0 <sutadev0@gmail.com>
2025-01-25 10:19:00 +0700

    feat: create LogoutBtnP.tsx

---
+++ components/LogoutBtnP.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { NeoButton } from "./ui/neo-button";
+import { toast } from "@/hooks/use-toast";
+import { Logout } from "@/server/actions";
+import { useRouter } from "next/navigation";
+
+const LogoutBtnP = () => {
+    const router = useRouter();
+
+  const handleLogout = async () => {
+    try {
+      const log = await Logout();
+
+      if (log.status === "Success") {
+        toast({
+          title: "Logout success",
+          description: "You have been logged out",
+        });
+
+        // Use router.push instead of redirect
+        router.push("/login");
+        // Force a page refresh to clear client state
+        router.refresh();
+      }
+    } catch (error) {
+      if (error) {
+        toast({
+          title: "Error",
+          description: "Something went wrong while logging out",
+          variant: "destructive",
+        });
+      }
+    }
+  };
+  return (
+    <NeoButton size="sm" className="transform rotate-1" onClick={handleLogout}>
+      Close Order
+    </NeoButton>
+  );
+};
+
+export default LogoutBtnP;
