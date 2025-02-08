55b1441ff7fe03ed89856e14c1b077e86a95a4c3
sutaaa0 <sutadev0@gmail.com>
2025-02-08 18:46:37 +0700

    update types.ts

--- types/types.ts
+++ types/types.ts
@@ -29,3 +29,29 @@ export interface Product {
     kuantitas: number;
     subtotal: number;
   }
+
+
+  // untuk di report & analisis
+export interface Penjualan {
+  penjualanId: number;
+  tanggalPenjualan: Date;
+  total_harga: number;
+  detailPenjualan: {
+    produk: Produk;
+  }[];
+}
+
+export interface Produk {
+  nama: string;
+  harga: number;
+  stok: number;
+  kategori: string;
+  isDeleted: boolean;
+}
+
+export interface Pelanggan {
+  nama: string;
+  points: number;
+  nomorTelepon?: string | null;
+  penjualan: Penjualan[];
+}
