71192229f91c02da468e41743e084551595d52a5
sutaaa0 <sutadev0@gmail.com>
2025-02-10 08:31:55 +0700

    feat: create migration.sql

---
+++ prisma/migrations/20250209105105_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `items` on the `Receipt` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Receipt" DROP COLUMN "items";
