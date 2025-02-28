eab6fd4f1d067d0699600ec1b297500cc0415204
sutaaa0 <sutadev0@gmail.com>
2025-02-28 17:26:37 +0700

    feat: create migration.sql

---
+++ prisma/migrations/20250228010736_add_harga_jual_harga_modal/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `harga` on the `Produk` table. All the data in the column will be lost.
+  - Added the required column `hargaJual` to the `Produk` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `hargaModal` to the `Produk` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Produk" DROP COLUMN "harga",
+ADD COLUMN     "hargaJual" INTEGER NOT NULL,
+ADD COLUMN     "hargaModal" INTEGER NOT NULL;
