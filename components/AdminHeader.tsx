1749e9c7554521669735f3ad0b9e82e3fc25229b
sutaaa0 <sutadev0@gmail.com>
2025-03-08 06:26:10 +0700

    update AdminHeader.tsx

--- components/AdminHeader.tsx
+++ components/AdminHeader.tsx
@@ -22,7 +22,7 @@ export function AdminHeader() {
         </div>
         <LogoutBtnP label="Logout" />
       </div>
-      <div className="text-3xl font-black transform -rotate-2 bg-[#FF6B6B] text-white p-2 border-3 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]">DASHBOARD</div>
+      <div className="text-3xl font-black transform -rotate-2 bg-[#93B8F3] text-white p-2 border-3 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]">DASHBOARD</div>
     </div>
   );
 }
