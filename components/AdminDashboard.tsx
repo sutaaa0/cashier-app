b1bb51d024ccf575242f9eada6dfd26003701948
sutaaa0 <sutadev0@gmail.com>
2025-03-08 06:26:07 +0700

    update AdminDashboard.tsx

--- components/AdminDashboard.tsx
+++ components/AdminDashboard.tsx
@@ -55,7 +55,7 @@ export default function AdminDashboard() {
       <PromotionAnalysis />
 
       {/* Products and Customers Section */}
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-10">
         {/* Top Selling Products */}
         <TopSellingProducts />
 
